3fa950995f507ed36e8a2de195bbaadbb0f1d5cb
Martin <this.frontend@gmail.com>
2017-07-15 11:56:11 +0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/index.js',
+  output: {
+    path: path.join(__dirname, 'server'),
+    filename: 'script.js'
+  },
+
+  resolve: {
+    extensions: [".webpack.js", ".web.js", ".js", ".json"]
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader?sourceMap&modules&importLoaders=1&localIdentName=[local]!postcss-loader' },
+      { test: /\.html$/, loader: 'text-loader' }
+    ]
+  }
+};
